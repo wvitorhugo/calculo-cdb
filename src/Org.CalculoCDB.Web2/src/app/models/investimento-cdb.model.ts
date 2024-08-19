14d458f22b9c69a4a45ff944f7d0b98b66c2308d
Wanderson de Paula <wvitorhugo@hotmail.com>
2024-08-19 02:38:40 -0300

    inicial

--- src/Org.CalculoCDB.Web2/src/app/models/investimento-cdb.model.ts
+++
@@ -1,9 +0,0 @@
-export class InvestimentoCDB {
-  Valor: number;
-  PrazoEmMeses: number;
-
-  constructor(valor: number, prazoEmMeses: number) {
-      this.Valor = valor;
-      this.PrazoEmMeses = prazoEmMeses;
-  }
-}
--- src/Org.CalculoCDB.Web2/src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    api: 'https://localhost:44334'
-};
